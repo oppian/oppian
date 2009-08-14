51a97d34ca724b4b50c711b533a6bb847f29003d
Matthew Jacobi <matt@oppian.com>
2009-08-14 01:22:26 +0100

    error handling when no tweets

--- apps/twitterapp/context_processors.py
+++ apps/twitterapp/context_processors.py
@@ -12,9 +12,11 @@ def latest_tweet(request):
 
     try:
         twitter = Twitter(settings.TWITTER_EMAIL, settings.TWITTER_PASSWORD, agent=settings.TWITTER_AGENT_STR)
-        tweet = twitter.statuses.friends_timeline(count=1)[0]
-        tweet['date'] = datetime.strptime(tweet['created_at'], "%a %b %d %H:%M:%S +0000 %Y")
-        cache.set('tweet', tweet, settings.TWITTER_TIMEOUT)
+        tweet = twitter.statuses.friends_timeline(count=1)
+        if tweet:
+            tweet = tweet[0]
+            tweet['date'] = datetime.strptime(tweet['created_at'], "%a %b %d %H:%M:%S +0000 %Y")
+            cache.set('tweet', tweet, settings.TWITTER_TIMEOUT)
     except TwitterError, e:
         pass
     
