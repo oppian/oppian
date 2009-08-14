6be2a174dcd30761609e3ede7155b1bb9cf478b2
Matthew Jacobi <matt@oppian.com>
2009-08-14 15:35:23 +0100

    fixing rightscripts (were reversed)
    also post install script now sets hostname

--- rightscripts/post-install.sh
+++ rightscripts/post-install.sh
@@ -3,12 +3,8 @@
 # Copyright (c) 2007-2009 RightScale, Inc, All Rights Reserved Worldwide.
 #
 # Parameters to retrieve the tarball with the source
-# $APPLICATION_CODE_BUCKET  -- Bucket where the tarball resides
-# $APPLICATION_CODE_PACKAGE -- S3 Key of the tarball  (gzipped)
-#
-# Ensure the S3 credentials are passed in the environment for the backup script to access the disk
-# $AWS_SECRET_ACCESS_KEY -- Amazon WS credential: secret access key
-# $AWS_ACCESS_KEY_ID     -- Amazon WS credential: access key
+# $DEPLOY_DIR  -- Installed directory
+# $HOSTNAME    -- Hostname of server
 
 #
 # Test for a reboot,  if this is a reboot just skip this script.
@@ -19,39 +15,14 @@ if test "$RS_REBOOT" = "true" ; then
   exit 0 # Leave with a smile ...
 fi
 
-echo "Checking whether APPLICATION_CODE_BUCKET and APPLICATION_CODE_PACKAGE are set..."
-if [ -z "$APPLICATION_CODE_BUCKET" -a -z "$APPLICATION_CODE_PACKAGE" ]; then 
-  echo "Bucket and prefix not defined..."
-  echo "skipping the retrieval of the application"
-  exit -1
-fi
+## set hostname
+hostname $HOSTNAME
 
-## Find out about the old deploy directory
-if [ -e $DEPLOY_DIR]; then
-  echo "Removing existing deploy directory..."
-  rm -rf $DEPLOY_DIR
-fi
-
-## Create deploy dir
-echo "Creating deploy directory..."
-mkdir -p $DEPLOY_DIR
-
-## Retrieve the code from S3 and unpack it
-echo "Downloading $APPLICATION_CODE_PACKAGE from S3:$APPLICATION_CODE_BUCKET..."
-s3cmd get $APPLICATION_CODE_BUCKET:$APPLICATION_CODE_PACKAGE /tmp/$APPLICATION_CODE_PACKAGE 
-
-## Prepare dir
-echo "Prepare to deploy..."
-chmod 775 $DEPLOY_DIR
-
-## Unpacking...
-echo "Unpacking web application..."
-echo "Extracting $APPLICATION_CODE_PACKAGE in $DEPLOY_DIR..."
-tar -xzf /tmp/$APPLICATION_CODE_PACKAGE -C $DEPLOY_DIR
-if [ ! $? ]; then
-  echo "Failed to extract sources."
-  exit -1
-fi
+## syncdb
+cd $DEPLOY_DIR
+chmod +x manage.py
+flip -u manage.py
+./manage.py syncdb --noinput
 
 ## chown
 echo "Changing ownership to $APP_OWNER..."
