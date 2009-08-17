44c2fa173559627e79178d59120df174a02bfbdf
Stephen Hartley <steve@oppian.com>
2009-08-17 13:02:27 +0100

    Added full exception capturing around the Twitter friend status fetch as it seems that Twitter can return bad data to the Twitter module, which barfs and brings everythoing down. So, instead I capture all exceptions, log them to stderr (the apache error log, when run remotely) and then continue. This should fix [#30 state:resolved].

--- apps/twitterapp/context_processors.py
+++ apps/twitterapp/context_processors.py
@@ -2,23 +2,23 @@ from datetime import datetime
 from django.conf import settings
 from django.core.cache import cache
 from twitter.api import Twitter, TwitterError
+import sys
 
- 
 def latest_tweet(request):
     tweet = cache.get('tweet')
 
-    if tweet:
-        return {"tweet": tweet}
-
-    try:
-        twitter = Twitter(settings.TWITTER_EMAIL, settings.TWITTER_PASSWORD, agent=settings.TWITTER_AGENT_STR)
-        tweet = twitter.statuses.friends_timeline(count=1)
-        if tweet:
-            tweet = tweet[0]
-            tweet['date'] = datetime.strptime(tweet['created_at'], "%a %b %d %H:%M:%S +0000 %Y")
-            cache.set('tweet', tweet, settings.TWITTER_TIMEOUT)
-    except TwitterError, e:
-        pass
-    
+    if not tweet:
+        try:
+            twitter = Twitter(settings.TWITTER_EMAIL, settings.TWITTER_PASSWORD, agent=settings.TWITTER_AGENT_STR)
+            tweet = twitter.statuses.friends_timeline(count=1)
+            if tweet:
+                tweet = tweet[0]
+                tweet['date'] = datetime.strptime(tweet['created_at'], "%a %b %d %H:%M:%S +0000 %Y")
+                cache.set('tweet', tweet, settings.TWITTER_TIMEOUT)
+        except:
+            log = 'exception when reading the twitter friends timeline (for "%s") - %s' %(settings.TWITTER_EMAIL, sys.exc_info()[0])
+            print >> sys.stderr, log
+            sys.stderr.flush()
+            pass
 
     return {"tweet": tweet}
