7035002f7ede8469c7f2237edb4e2d51348ece54
Matthew Jacobi <matt@oppian.com>
2009-08-19 00:17:34 +0100

    updating production password

--- settings_production.py
+++ settings_production.py
@@ -5,6 +5,6 @@ TEMPLATE_DEBUG = DEBUG
 DATABASE_ENGINE = 'postgresql_psycopg2'           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
 DATABASE_NAME = 'oppian'              # Or path to database file if using sqlite3.
 DATABASE_USER = 'oppian'             # Not used with sqlite3.
-DATABASE_PASSWORD = 'test1'         # Not used with sqlite3.
+DATABASE_PASSWORD = 'ceelness22'         # Not used with sqlite3.
 DATABASE_HOST = 'localhost'             # Set to empty string for localhost. Not used with sqlite3.
 DATABASE_PORT = ''             # Set to empty string for default. Not used with sqlite3.
