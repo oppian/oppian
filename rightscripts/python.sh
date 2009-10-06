7e94694cf4ea8da4511783aeee8039f1cb268acc
Stephen Hartley <steve@oppian.com>
2009-10-06 01:55:26 +0100

    Adding pygments to the list of easy_install modules

--- rightscripts/python.sh
+++ rightscripts/python.sh
@@ -1,11 +1,10 @@
 #!/bin/bash -e
 #
 # Easy Install
-#
-# pkgs: python python-setuptools python-dateutil python-psycopg2
 
 easy_install twitter
 easy_install markdown
 easy_install beautifulsoup
+easy_install pygments
 
 exit 0
