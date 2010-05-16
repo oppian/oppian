4d79e3f86093dcdb9467152c4c75b0fc1214278d
oppianmatt <matt@oppian.com>
2010-05-16 22:41:27 +0100

    Removing the redundant build app middleware use

--- settings.py
+++ settings.py
@@ -106,7 +106,6 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'build.middleware.BuildMiddleware', # insert build version into headers
 )
 
 ROOT_URLCONF = 'urls'
