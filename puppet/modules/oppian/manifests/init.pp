a42793bebf538e5f1a5018bd432e6014e9a95942
Matthew Jacobi <matt@oppian.com>
2012-04-03 18:01:45 +0100

    fixing up puppet script to not have mysql in it (moved to base classes)

--- puppet/modules/oppian/manifests/init.pp
+++ puppet/modules/oppian/manifests/init.pp
@@ -1,13 +1,7 @@
 class oppian {
 
-  package { "system-release" :
-    ensure => 'latest',
-  }
-  
-  package { "mysql": }
-  package { "mysql-devel": 
-    require => Package["mysql"],
-  }
+  $project = "oppianproj"
+  $debug_django = "True"
 
   class { "webapp::python": owner => "root",
                           group => "wheel",
@@ -15,10 +9,9 @@ class oppian {
                           nginx_workers => 2,
                           monit_admin => "matt@oppian.com",
                           monit_interval => 30,
-                          require => [Package["mysql-devel"], Package['system-release']],
   }
   
-  webapp::python::instance { "oppianproj":
+  webapp::python::instance { $project:
     domain => "oppian.com",
     django => true,
     requirements => true,
@@ -26,9 +19,7 @@ class oppian {
     pythonpath => ["lib/django", "apps", "apps/oppianapp/utils", "lib/django-storages"],
   }
   
-  $debug_django = "False"
-  
-  file { "/deploy/oppianproj/settings_local.py":
+  file { "/deploy/$project/settings_local.py":
     ensure => file,
     content => template('oppian/settings.py.erb'),
   }
