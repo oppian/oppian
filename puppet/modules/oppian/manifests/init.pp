f62cdd6f2b927a860a05b5ce5d69bca4f591619e
Matthew Jacobi <matt@oppian.com>
2012-04-04 00:40:29 +0100

    adding varnish and worker count to puppet

--- puppet/modules/oppian/manifests/init.pp
+++ puppet/modules/oppian/manifests/init.pp
@@ -6,22 +6,26 @@ class oppian {
   class { "webapp::python": owner => "root",
                           group => "wheel",
                           src_root => "/deploy",
-                          nginx_workers => 2,
+                          nginx_workers => 1,
                           monit_admin => "matt@oppian.com",
                           monit_interval => 30,
   }
-  
+
   webapp::python::instance { $project:
+    workers => inline_template("<%= (processorcount.to_i * 2 + 1) -%>"),
+    listen_port => 8080,
     domain => "oppian.com",
     django => true,
     requirements => true,
     django_syncdb => true,
     pythonpath => ["lib/django", "apps", "apps/oppianapp/utils", "lib/django-storages"],
   }
-  
+
   file { "/deploy/$project/settings_local.py":
     ensure => file,
     content => template('oppian/settings.py.erb'),
   }
+
+  class {'varnish':}
 
 }
