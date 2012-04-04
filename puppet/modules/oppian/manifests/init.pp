9548e00008ddb9d9e84cd3913f1c90f7f8fe32cf
Matthew Jacobi <matt@oppian.com>
2012-04-04 23:15:22 +0100

    adding media root and prefix to nginx

--- puppet/modules/oppian/manifests/init.pp
+++ puppet/modules/oppian/manifests/init.pp
@@ -18,12 +18,24 @@ class oppian {
     django => true,
     requirements => true,
     django_syncdb => true,
+    mediaroot => "/deploy/$project/media",
+    mediaprefix => "/m",
     pythonpath => ["lib/django", "apps", "apps/oppianapp/utils", "lib/django-storages"],
   }
 
   file { "/deploy/$project/settings_local.py":
     ensure => file,
     content => template('oppian/settings.py.erb'),
+  }
+  
+  file { "/deploy/$project/media/admin":
+  	ensure => link,
+  	target => "/deploy/$project/lib/django/django/contrib/admin/media"
+  }
+  
+  file { "/deploy/$project/media/favicon.ico":
+  	ensure => link,
+  	target => "/deploy/$project/media/images/favicon.ico"
   }
 
   class {'varnish':}
