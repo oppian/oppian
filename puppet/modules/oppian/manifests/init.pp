a5efe7269cc06acf6cfb01598edfaa7534b1d358
Matthew Jacobi <matt@oppian.com>
2012-04-10 15:30:45 +0100

    adding hostname and debug to puppet

--- puppet/modules/oppian/manifests/init.pp
+++ puppet/modules/oppian/manifests/init.pp
@@ -1,20 +1,27 @@
 class oppian {
 
   $project = "oppianproj"
-  $debug_django = "True"
 
   class { "webapp::python": owner => "root",
                           group => "wheel",
                           src_root => "/deploy",
                           nginx_workers => 1,
-                          monit_admin => "matt@oppian.com",
+                          monit_admin => $cfn_adminemail,
                           monit_interval => 30,
+  }
+  
+  if $cfn_hostname2 {
+  	$aliases = [$cfn_hostname2]
+  }
+  else {
+  	$aliases = []
   }
 
   webapp::python::instance { $project:
     workers => inline_template("<%= (processorcount.to_i * 2 + 1) -%>"),
     listen_port => 8080,
-    domain => "oppian.com",
+    domain => $cfn_hostname,
+    aliases => $aliases,
     django => true,
     requirements => true,
     django_syncdb => true,
